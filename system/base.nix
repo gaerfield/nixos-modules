737898dc6f06ee58b78fe57832efe27ea440ed3c
gaerfield <gaerfield@users.noreply.github.com>
2025-01-07 09:28:47 +0100

    rename option `hardware.pulseaudio` to `services.pulseaudio`

--- system/base.nix
+++ system/base.nix
@@ -149,7 +149,7 @@ in {
     ];
 
     # Enable sound with pipewire.
-    hardware.pulseaudio.enable = false;
+    services.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;

b95f7445044b67fbc7c5302fe6573cbbb3317501
gaerfield <gaerfield@users.noreply.github.com>
2025-02-03 14:39:11 +0100

    allow simulating tpm in virtual machines

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -1,10 +1,12 @@
-{ config, ... }: 
+{ config, pkgs, ... }: 
 let 
   mainuser = config.system.username;
 in {
   # https://nixos.wiki/wiki/Virt-manager
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+
+  environment.systemPackages = with pkgs; [ swtpm ];
 
   users.users."${mainuser}".extraGroups = [ "libvirtd" ];
 

aa2658cd1fc76673fb7749fd8ad2f8af61aa79a7
gaerfield <gaerfield@users.noreply.github.com>
2025-05-20 15:33:41 +0200

    include jj into stanard set of tools

--- hm/base/git.nix
+++ hm/base/git.nix
@@ -1,15 +1,35 @@
-{
+let
+  user = {
+    name = "gaerfield";
+    email = "gaerfield@users.noreply.github.com";
+  };
+in {
   # basic configuration of git
   programs.git = {
-    enable = true;
-    userName = "gaerfield";
-    userEmail = "gaerfield@users.noreply.github.com";
-  
-    extraConfig = {
-      core = {
-        sshCommand = "ssh -i ~/.ssh/gaerfield";
+      enable = true;
+      userName = user.name;
+      userEmail = user.email;
+    
+      extraConfig = {
+        core = {
+          sshCommand = "ssh -i ~/.ssh/gaerfield";
+        };
+        init.defaultBranch = "main";
       };
-      init.defaultBranch = "main";
+  };
+
+  programs.jujutsu = {
+    enable = true;
+
+    # https://github.com/jj-vcs/jj/blob/main/docs/config.md
+    settings = {
+      user = {
+        name = user.name;
+        email = user.email;
+      };
+      ui = {
+        default-command = "log";
+      };
     };
   };
 }

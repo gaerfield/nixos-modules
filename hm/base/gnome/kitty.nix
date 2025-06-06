2f019b39f1c42a7fad65a1246a151e44028c9218
gaerfield <gaerfield@users.noreply.github.com>
2025-06-06 08:17:27 +0200

    disable dynamic background opacity for kitty

--- hm/base/gnome/kitty.nix
+++ hm/base/gnome/kitty.nix
@@ -7,7 +7,7 @@
     # https://sw.kovidgoyal.net/kitty/conf/
     settings = {
       confirm_os_window_close = 0;
-      dynamic_background_opacity = true;
+      dynamic_background_opacity = false;
       enable_audio_bell = false;
       mouse_hide_wait = "-1.0";
       window_padding_width = 10;

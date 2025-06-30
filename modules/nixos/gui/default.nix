4054e1afb3519ab4b26db52604a86b33652de080
gaerfield <gaerfield@users.noreply.github.com>
2025-06-30 14:46:27 +0200

    drop autologin option

--- modules/nixos/gui/default.nix
+++ modules/nixos/gui/default.nix
@@ -6,11 +6,5 @@
 
   options.gnm.gui = {
     enable = mkEnableOption "Enable the GNOME GUI.";
-    autologin = mkEnableOption "Enable automatic login for the main user.";
-    mainuser = mkOption {
-      type = types.str;
-      default = "nixos";
-      description = "The main user for the GNOME GUI.";
-    };
   };
 }

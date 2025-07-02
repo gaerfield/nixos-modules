14144b5ea1edeeea59f77de8e88684114e1b01c8
gaerfield <gaerfield@users.noreply.github.com>
2025-07-02 08:06:56 +0200

    enable nixos helper

--- modules/home/base/nix.nix
+++ modules/home/base/nix.nix
@@ -1,7 +1,34 @@
-{inputs, pkgs, ...}: {
-  # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
+{inputs, pkgs, lib, ...}: with lib; let
+  cfg = inputs.gnm.hm.nix;
+in {
+  imports = [
+    inputs.nix-index-database.hmModules.nix-index
+  ];
 
-  home.packages = with pkgs; [alejandra nixd];
-  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+  options.gnm.hm.nix = {
+    flakePath = mkOption {
+      type = types.str;
+      default = "";
+      example = "/home/user/my-nixos-config";
+      description = "Path to the nixos flake configuration (used by 'nh').";
+    };
+  };
+
+  config = {
+    # Nicely reload system units when changing configs
+    systemd.user.startServices = "sd-switch";
+
+    home.packages = with pkgs; [alejandra nixd];
+    nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+
+    programs.nix-index.enable = true;
+
+    # nixos-helper: https://github.com/nix-community/nh
+    programs.nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
+      flake = cfg.flakePath;
+    };
+  };
 }

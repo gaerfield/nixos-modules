d1737dac1512557e92fbdd66570c88c16e977670
gaerfield <gaerfield@users.noreply.github.com>
2025-07-23 20:48:46 +0200

    info comment on boot selection menu

--- modules/nixos/os/boot.nix
+++ modules/nixos/os/boot.nix
@@ -1,7 +1,7 @@
 {
   boot.loader = {
     systemd-boot.enable = true;
-    timeout = 0;
+    timeout = 0; # skip selection menu, unless <space> is pressed during boot
     efi = {
       canTouchEfiVariables = true;
       efiSysMountPoint = "/boot";

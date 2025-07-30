110e3c76671b30625c1f02e2cd4ae82638a2c836
gaerfield <gaerfield@users.noreply.github.com>
2025-07-30 11:43:34 +0200

    fiddle with podman abbreviations

--- modules/nixos/containers/default.nix
+++ modules/nixos/containers/default.nix
@@ -24,8 +24,6 @@ in {
       podman = {
         enable = true;
 
-        # Create a `docker` alias for podman, to use it as a drop-in replacement
-        dockerCompat = true;
         dockerSocket.enable = true;
         # Required for containers under podman-compose to be able to talk to each other.
         defaultNetwork.settings.dns_enabled = true;
@@ -49,8 +47,10 @@ in {
     cfg.users;
 
     programs.fish = {
-      shellAbbrs.p = "podman";
+      shellAbbrs.po = "podman";
+      shellAbbrs.docker = "podman";
       shellAbbrs.dco = "podman compose";
+      shellAbbrs.pco = "podman compose";
     };
   };
 }

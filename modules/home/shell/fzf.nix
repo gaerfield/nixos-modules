21bfa18f487f859069fb3064d495ba1b4ec4be67
gaerfield <gaerfield@users.noreply.github.com>
2025-08-20 11:28:18 +0200

    pimp fzf

--- modules/home/shell/fzf.nix
+++ modules/home/shell/fzf.nix
@@ -5,19 +5,29 @@
     enableBashIntegration = true;
     enableFishIntegration = true;
     enableZshIntegration = true;
-
-    # colors = {
-    #   hl = colors.bright.accent;
-    #   "hl+" = colors.normal.accent;
-    #   info = colors.normal.green;
-    #   prompt = colors.normal.white;
-    #   pointer = colors.normal.accent;
-    # };
-
-    # historyWidgetOptions = [ "--no-multi" ];
+    defaultOptions = [
+      "--info=inline"
+      "--preview='bat -n --color=always {}'"
+      "--border=rounded"
+      "--margin=1"
+      "--padding=1"
+    ];
+    # regarding options: https://github.com/junegunn/fzf?tab=readme-ov-file#key-bindings-for-command-line
+    # alt-c options
+    changeDirWidgetOptions = [
+      "--preview 'eza --group-directories-first --no-quotes --no-permissions --no-user --no-git --tree --level=2 --color=always {}'"
+    ];
+    # ctrl-t options
+    fileWidgetOptions = [
+      "--preview='bat -n --color=always {}'"
+      "--bind 'ctrl-/:change-preview-window(down|hidden|)'"
+    ];
+    # ctrl-r options
+    historyWidgetOptions = [ "--no-multi" ];
   };
 
   programs.fish = {
+    # is this even still needed?
     plugins = with pkgs.fishPlugins; [
       {
         name = "fzf";
@@ -29,9 +39,9 @@
       } # requires fd and bat
     ];
     shellAbbrs.fzf-help = "fzf_configure_bindings --help";
+    # ctrl-o = edit file, ctrl-g = xdg-open
     interactiveShellInit = ''
       set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
-      set fzf_directory_opts --bind "ctrl-x:execute(xdg-open {} &> /dev/tty)"
     '';
   };
 

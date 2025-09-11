4d918756956fcef989406c2fe97a068ca9ed397b
gaerfield <gaerfield@users.noreply.github.com>
2025-09-11 08:20:18 +0200

    adapt some yazi default config

--- modules/home/shell/yazi.nix
+++ modules/home/shell/yazi.nix
@@ -7,6 +7,22 @@
         show_hidden = false;
         sort_by = "natural";
         sort_dir_first = true;
+        prepend_keymap = [
+          {
+            # when yank, put files into the clipboard as well
+            on  = "y";
+            run = [ 
+              "shell -- for path in \"$@\"; do echo \"file://$path\"; done | wl-copy -t text/uri-list"
+              "yank"
+            ];
+          }
+        ];
+      };
+      tasks = {
+        # maximum 250MB allocation when opening an image
+        # no other size restrictions
+        image_alloc = 250000000; 
+        image_bound = 0;
       };
     };
   };

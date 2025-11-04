c5f0ab5e2d254d4065a8c6cf5dc3fd947a9bdb26
gaerfield <gaerfield@users.noreply.github.com>
2025-11-04 13:22:38 +0100

    properly set xdg

--- modules/home/base/xdg.nix
+++ modules/home/base/xdg.nix
@@ -1,5 +1,14 @@
-{config, ...}: {
-  xdg.enable = true;
+{config, pkgs, ...}: {
+  xdg = {
+    enable = true;
+    portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      extraPortals = [ pkgs.xdg-desktop-portal ];
+      config.common.default = "*";
+    };
+  };
+  home.packages = [ pkgs.xdg-utils ];
   home.sessionVariables = {
     XDG_BIN_HOME = "${config.home.homeDirectory}/.local/bin";
   };

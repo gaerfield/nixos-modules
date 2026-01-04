82afb7db079c7e200220b0c915b7dc30edef9a50
gaerfield <gaerfield@users.noreply.github.com>
2026-01-04 21:57:27 +0100

    update lockfile

--- modules/home/java-development/default.nix
+++ modules/home/java-development/default.nix
@@ -7,10 +7,10 @@
 with lib; let
   cfg = config.gnm.hm.javaDevelopment;
 in {
-  options.gnm.hm.javaDevelopment.enable = mkEnableOption "java dvelopment tools";
+  options.gnm.hm.javaDevelopment.enable = mkEnableOption "java development tools";
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [temurin-bin jetbrains.idea-ultimate jetbrains.datagrip];
+    home.packages = with pkgs; [temurin-bin jetbrains.idea jetbrains.datagrip];
     home.sessionVariables = {
       GRADLE_USER_HOME = "${config.xdg.dataHome}/gradle";
     };

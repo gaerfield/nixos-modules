7ec22a4a4478294396296863f5fa3d470f08f680
gaerfield <gaerfield@users.noreply.github.com>
2026-01-17 01:23:42 +0100

    update flake

--- modules/nixos/networking/dns-resolve.nix
+++ modules/nixos/networking/dns-resolve.nix
@@ -3,22 +3,22 @@
   networking.nameservers = ["1.1.1.1#cloudflare-dns.com" "2606:4700:4700::1111#cloudflare-dns.com"];
   services.resolved = {
     enable = true;
-    dnssec = "allow-downgrade";
-    domains = ["~."];
-    fallbackDns = [
-      "1.0.0.1#cloudflare-dns.com"
-      "2606:4700:4700::1001#cloudflare-dns.com"
-      "9.9.9.9#dns.quad9.net"
-      "2620:fe::fe#dns.quad9.net"
-      "8.8.8.8#dns.google"
-      "2001:4860:4860::8888#dns.google"
-    ];
     # https://www.guyrutenberg.com/2023/03/14/split-dns-using-systemd-resolved/
-    dnsovertls = "opportunistic"; # allow fallback for corporate machines
-    llmnr = "false";
-    extraConfig = ''
-      MulticastDNS=true
-    '';
+    settings.Resolve = {
+      DNSSEC = "allow-downgrade";
+      DOMAINS = ["~."];
+      FallbackDNS = [
+        "1.0.0.1#cloudflare-dns.com"
+        "2606:4700:4700::1001#cloudflare-dns.com"
+        "9.9.9.9#dns.quad9.net"
+        "2620:fe::fe#dns.quad9.net"
+        "8.8.8.8#dns.google"
+        "2001:4860:4860::8888#dns.google"
+      ];
+      DNSOverTLS = "opportunistic"; # allow fallback for corporate machines
+      LLMNR = false;
+      MulticastDNS = true;
+    };
   };
   # turn off DNSSEC for WIFIonICE, as it is otherwise impossible to open "https://login.wifionice.de"
   # to agree with the terms of usage

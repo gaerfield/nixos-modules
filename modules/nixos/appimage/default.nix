b16e4242c05f055564281e0ed6a5e04745463272
gaerfield <gaerfield@users.noreply.github.com>
2026-04-13 09:05:52 +0200

    correctly reference libxshmfence

--- modules/nixos/appimage/default.nix
+++ modules/nixos/appimage/default.nix
@@ -17,7 +17,7 @@ in {
       package = pkgs.appimage-run.override {
         extraPkgs = pkgs: [
           # missing libraries here, e.g.: `pkgs.libepoxy`
-          pkgs.xorg.libxshmfence
+          pkgs.libxshmfence
         ];
       };
     };
